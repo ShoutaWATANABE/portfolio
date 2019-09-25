661e06a87e0e0dc4144bb07dcce905610c6aab71
ShoutaWATANABE <s06w12@gmail.com>
2019-09-25 11:09:50 +0900

    [change] twitter cardテスト

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,31 @@ module.exports = {
         name: 'description',
         content:
           '渡邉将大のポートフォリオサイトです。仙台で社内エンジニアとして勤めております。'
-      }
+      },
+      { hid: 'twitter:card', name: 'twitter:card', content: 'summary' },
+      { hid: 'twitter:site', name: 'twitter:site', content: '@_ShoutaWATANABE' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'ShoutaWATANABE Portfolio'
+      },
+      {
+        hid: 'og:url',
+        property: 'og:url',
+        content: 'https://shoutawatanabe-portfolio.netlify.com'
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: '渡邉将大のポートフォリオサイトです。仙台で社内エンジニアとして勤めております。'
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content: '~/assets/images/card.png'
+      },
+      { hid: 'og:site_name', name: 'og:site_name', content: 'ShoutaWATANABE Portfolio' }
     ],
     link: [
       {
