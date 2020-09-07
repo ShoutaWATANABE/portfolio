0ecb547dd2e5940ecb7b71fd931e7bf2e09ceef3
ShoutaWATANABE <s06w12@gmail.com>
2020-09-07 18:40:06 +0900

    [change] CORS

--- pages/blog.vue
+++ pages/blog.vue
@@ -27,33 +27,11 @@ export default {
         ? '/api/'
         : 'https://blog.shoutawatanabe.info/wp-json/wp/v2/'
     const itemList = []
-    const postsData = await axios
-      .create({
-        withCredentials: process.env.NODE_ENV !== 'production',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,DELETE',
-          'Access-Control-Allow-Headers': 'x-requested-with',
-          'X-Requested-With': 'XMLHttpRequest'
-        },
-        timeout: 20000
-      })
-      .get(url + 'posts?per_page=5&_envelope')
+    const postsData = await axios.get(url + 'posts?per_page=5&_envelope')
     // const tags_data = await axios.get(url + 'tags?per_page=100&_envelope')
-    const categoriesData = await axios
-      .create({
-        withCredentials: process.env.NODE_ENV !== 'production',
-        headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'POST,GET,OPTIONS,DELETE',
-          'Access-Control-Allow-Headers': 'x-requested-with',
-          'X-Requested-With': 'XMLHttpRequest'
-        },
-        timeout: 20000
-      })
-      .get(url + 'categories?per_page=100&_envelope')
+    const categoriesData = await axios.get(
+      url + 'categories?per_page=100&_envelope'
+    )
     for (const post of postsData.data.body) {
       let categoryGroup = []
       if (post.categories.length > 0) {
